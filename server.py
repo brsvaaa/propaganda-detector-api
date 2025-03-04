77920d7e0be5799fccf0d6afdebedd374f8bc1c4
Alena Borisova <borisova.alena@seznam.cz>
2025-03-04 12:17:46 +0100

    Update server.py

--- server.py
+++ server.py
@@ -187,4 +187,6 @@ def predict():
     return jsonify({"results": results})
     '''
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
